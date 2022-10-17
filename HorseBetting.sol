49041f2f4544d629a36e268e5db03e88ac54322a
Hernan Camilo Rivera Arteaga <camilorivera108@gmail.com>
2022-10-16 23:45:27 -0500

    Plus: Primera versión de metodo para registrar un a un caballo en una carrera

--- HorseBetting.sol
+++ HorseBetting.sol
@@ -36,8 +36,10 @@ contract HorseBetting {
     // Guardar el codigo del caballo y devolver la posicion en la que quedó en la lista horses
     mapping(uint256 => uint256) public horseCodeToHorsesListIndex;
 
-    mapping(uint256 => Horse[]) public careerCodeToHorses;
+    // Register a horse(Horse object) in a career(careerCode)
     mapping(uint256 => Career[]) public horseCodeToCareers;
+    // Join a career(Career object) with a horse(horseCode) to find how many horses have a career
+    mapping(uint256 => Horse[]) public careerCodeToHorses;
 
     
     constructor() {
@@ -69,7 +71,7 @@ contract HorseBetting {
      * @param name value to career
      */
     function registerHorse(uint256 code, string memory name) public  {
-        // TODO: Validate career code to know if it doesn't exist already
+        // TODO: Validate horse code to know if it doesn't exist already
         horseCodeToHorsesListIndex[code] = horses.length;
         horses.push(
             Horse({
@@ -77,6 +79,34 @@ contract HorseBetting {
                 name: name
             })
         );
+    }
+
+    /**
+     * @dev Register a Horse in a Career wiht CREATED status
+     * @param horseCode value of code horse
+     * @param careerCode value of code career
+     */
+    function registerHorseInCareer(uint256 horseCode, uint256 careerCode) public  {
+        // TODO: Validate horse code to know if it doesn't exist already
+        // Find Career object
+        uint256 careerCodeListIndex = careerCodeToCareersListIndex[careerCode];
+        Career storage careerObj = careers[careerCodeListIndex];
+        
+        // Find Horse object
+        uint256 horseCodeListIndex = horseCodeToHorsesListIndex[horseCode];
+        Horse storage horseObj = horses[horseCodeListIndex];
+
+        // TODO: Validate if the career has a number greater than 5 and less than 2 horses
+        // TODO: Validate if the career has CREATED status
+
+        // Add a career to the horse
+        Career[] storage careersPerHorse = horseCodeToCareers[horseCode];
+        careersPerHorse.push(careerObj);
+
+        // Add a horse to the career
+        Horse[] storage horsesPerCareer = careerCodeToHorses[careerCode];
+        horsesPerCareer.push(horseObj);
+
     }
 
 }
